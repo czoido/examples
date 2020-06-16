bbc5ce2a209b8f688b9fb42162f53dc72fd16bfb
czoido <mrgalleta@gmail.com>
2020-06-16 09:02:59 +0200

    wip

--- .ci/run.py
+++ .ci/run.py
@@ -54,7 +54,7 @@ def get_examples_to_skip(current_version):
     skip = []
     # Given the Conan version, some examples are skipped
     required_conan = {
-        version.parse("1.23.0"): [
+        version.parse("1.24.0"): [
             './libraries/folly/basic',  # Requires fix related to import cppstd_flag in boost
             './libraries/poco/md5',  # Uses get_safe() with 3 arguments
             './features/deployment',  # Fails because of poco requirement
@@ -86,6 +86,7 @@ def get_examples_to_skip(current_version):
 
 def get_build_list():
     skip_examples = get_examples_to_skip(current_version=version.parse(conan_version))
+    print("***** skipping examples: *****")
     print(skip_examples)
     builds = []
     script = "build.bat" if platform.system() == "Windows" else "build.sh"
